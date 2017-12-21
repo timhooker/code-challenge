c5c6d0a3b3279075d88c42db81b8c2deeb0631ed
Tim Hooker <timhooker@me.com>
2017-12-21 15:00:09 -0500

    commenting and reordering python number game

--- number_game.py
+++ number_game.py
@@ -1,18 +1,7 @@
 import math
 
-def number_game(limit:int):
-    state = {
-        'finished': False,
-        'guesses': 0,
-        'start': 0,
-        'end': limit
-    }
-    print('Enter h for high, l for low, and c for correct ')
-    while(state['finished'] is False):
-        state = take_a_guess(state)
-    return state
-
 def game_board():
+    """Intoduction to the game"""
     limit = int(input('Let\'s play a game. \nPlease enter a number greater than 0: '))
     if(limit <= 0):
         raise ValueError(f"{limit} is not greater than 0")
@@ -25,10 +14,22 @@ def game_board():
     print(f"{'Success:' if result['guesses'] <= goal else 'Fail:'} It took me {result['guesses']} guesses")
     return True if (input("Play again (y/n)? ") in 'yY') else False
 
-def binary_search(start:int, end:int):
-    return int(math.ceil((end - start) / 2 + start))
+def number_game(limit:int):
+    """Setting up game parameters and giving instructions before we start"""
+    state = {
+        'finished': False,
+        'guesses': 0,
+        'start': 0,
+        'end': limit
+    }
+    print('Enter h for high, l for low, and c for correct ')
+    while(state['finished'] is False):
+        state = take_a_guess(state)
+    return state
+
 
 def take_a_guess(state):
+    """"logic for guessing numbers and getting feedback"""
     guess = binary_search(state['start'], state['end'])
     state['guesses'] += 1
     if(state['end'] - state['start'] <= 1):
@@ -39,7 +40,13 @@ def take_a_guess(state):
         response = input(f"Is your number {guess}? ")
     return update_state(state, guess, response)
 
+
+def binary_search(start:int, end:int):
+    """Utility Function to divide search point"""
+    return int(math.ceil((end - start) / 2 + start))
+
 def update_state(state, guess, response):
+    """Updating internal state of game based on user response"""
     if(response is 'h'):
         state['end'] = guess
     elif(response is 'l'):
