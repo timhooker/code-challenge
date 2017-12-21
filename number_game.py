06da38e2e974785b3a750490141cd918e3cf6ac2
Tim Hooker <timhooker@me.com>
2017-12-21 14:25:47 -0500

    put init of number_game into function

--- number_game.py
+++ number_game.py
@@ -12,6 +12,19 @@ def number_game(limit:int):
         state = take_a_guess(state)
     return state
 
+def game_board():
+    limit = int(input('Let\'s play a game. \nPlease enter a number greater than 0: '))
+    if(limit <= 0):
+        raise ValueError(f"{limit} is not greater than 0")
+    print(f'Great! Now choose a number between 0 and {limit}.')
+    goal = int(math.log2(limit)) + 1
+    print(f"I'll get it in {goal} guesses")
+    input('When you are ready press enter... ')
+    result = number_game(limit)
+    print(f"Your number is {result['number']}")
+    print(f"{'Success:' if result['guesses'] <= goal else 'Fail:'} It took me {result['guesses']} guesses")
+    return True if (input("Play again (y/n)? ") in 'yY') else False
+
 def binary_search(start:int, end:int):
     return int(math.ceil((end - start) / 2 + start))
 
@@ -36,16 +49,9 @@ def update_state(state, guess, response):
         state['number'] = guess
     return state
 
-play = True
-while(play):
-    limit = int(input('Let\'s play a game. \nPlease enter a number greater than 0: '))
-    if(limit <= 0):
-        raise ValueError(f"{limit} is not greater than 0")
-    print(f'Great! Now choose a number between 0 and {limit}.')
-    goal = int(math.log2(limit)) + 1
-    print(f"I'll get it in {goal} guesses")
-    input('When you are ready press enter... ')
-    result = number_game(limit)
-    print(f"Your number is {result['number']}")
-    print(f"{'Success:' if result['guesses'] <= goal else 'Fail:'} It took me {result['guesses']} guesses")
-    play = True if (input("Play again (y/n)? ") in 'yY') else False
+def init():
+    play = True
+    while(play):
+        play = game_board()
+
+init()
